5a21f04bcabcb6b8b97ce289fb9437d3c4808046
yurec2306 <yuro4ka2306@gmail.com>
2017-05-25 09:59:46 +0300

    add registration window and controller

---
+++ credit-helper-beta/src/main/java/main/login/LoginController.java
@@ -0,0 +1,55 @@
+package main.login;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import main.accounts.AccountModel;
+import main.registration.RegistrationController;
+
+public class LoginController {
+	
+	private LoginWindow window;
+	private AccountModel model;
+	
+
+	public void init() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window = new LoginWindow();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				window.lblRegistration.addMouseListener(new MouseAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent arg0) {
+						new RegistrationController().init();
+					}
+				});
+				window.btnNext.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						if(checkAccountData(window.tfLogin.getText(), window.pfPassword.getPassword())) {
+							window.setVisible(false);
+							window.dispose();
+							//go to main menu
+						} else {
+							window.tfLogin.setText("");
+							window.pfPassword.setText("");
+							window.lblError.setVisible(true);
+						}
+					}
+				});
+			}
+		});
+	}
+	
+	private boolean checkAccountData(String login, char[] password) {
+		//check account data
+		return true;
+	}
+
+}
---
+++ credit-helper-beta/src/main/java/main/registration/RegistrationController.java
@@ -0,0 +1,70 @@
+package main.registration;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import main.accounts.AccountModel;
+import main.accounts.AccountModel.UserType;
+
+public class RegistrationController {
+	
+	private RegistrationWindow window;
+	private AccountModel model;
+	
+	public RegistrationController() {
+		model = new AccountModel();
+	}
+	
+	public void init() {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					window = new RegistrationWindow();
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				window.btnNext.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						if(fieldsIsEmpty()) {
+							setToModel(model);
+							
+							// save data to db there
+							
+							window.setVisible(false);
+							window.dispose();
+							
+							// success window
+						}
+					}
+				});
+			}
+		});
+	}
+	
+	private boolean fieldsIsEmpty() {
+		if (!(window.tfFirstName.getText().isEmpty()    ||
+				window.tfMiddleName.getText().isEmpty() ||
+				window.tfLastName.getText().isEmpty()   ||
+				window.tfPhone.getText().isEmpty()      ||
+				window.tfBunkNumber.getText().isEmpty() ||
+				window.tfLogin.getText().isEmpty()      ||
+				window.tfPassword.getPassword().length == 0)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private void setToModel(AccountModel model) {
+		model.setFirstName(window.tfFirstName.getText());
+		model.setLastName(window.tfLastName.getText());
+		model.setMiddleName(window.tfMiddleName.getText());
+		model.setPhone(window.tfPhone.getText());
+		model.setBankNumber(window.tfBunkNumber.getText());
+		model.setLogin(window.tfLogin.getText());
+		model.setPassword(window.tfPassword.getPassword());
+		model.setUserType(UserType.USER);
+	}
+
+}
