ee76a3dc2051b41da0c5498564e9918efb127133
NetLagina <gan.p@mail.ru>
2017-05-30 14:23:37 +0300

    a

--- credit-helper-beta/src/test/java/dataBase/AccountDaoTest.java
+++ credit-helper-beta/src/test/java/dataBase/AccountDaoTest.java
@@ -4,11 +4,19 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.lang.annotation.Annotation;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.internal.SessionFactoryImpl;
 import org.hibernate.internal.SessionImpl;
 import org.junit.Test;
+
+import com.fasterxml.classmate.AnnotationConfiguration;
+import com.fasterxml.classmate.AnnotationInclusion;
+
 import main.accounts.AccountModel;
 
 public class AccountDaoTest {
@@ -16,13 +24,13 @@ public class AccountDaoTest {
 	@Test
 	public void testSaveOrUpdate() {
 		AccountDAO dao = new AccountDAO();
-		SessionFactory sessionFactoryMock = mock(SessionFactoryImpl.class);
-		Session session = mock(SessionImpl.class);
-		when(sessionFactoryMock.openSession()).thenReturn(session);
-		dao.setSessionFactory(sessionFactoryMock);
+		File f = new File("..\\HibernateTest.cfg.xml");
+		SessionFactory sessionFactory = new Configuration().configure(f).buildSessionFactory();
+		dao.setSessionFactory(sessionFactory);
 		AccountModel model = new AccountModel();
 		model.setFirstName("Test Name");
 		dao.saveOrUpdate(model);
+		dao.close();
 	}
 	
 	@Test
