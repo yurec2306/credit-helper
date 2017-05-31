5aab2456bc9272a91f6cbcb64e8a2ed215bfe894
NetLagina <gan.p@mail.ru>
2017-05-31 13:25:06 +0300

    fix

--- credit-helper-beta/src/main/java/creditHelper/Step4IndividualController.java
+++ credit-helper-beta/src/main/java/creditHelper/Step4IndividualController.java
@@ -14,13 +14,13 @@ import main.ErrorWindow;
 import main.IndividualModel;
 
 public class Step4IndividualController extends AbstractStepController {
-	
+
 	private static Logger logger = LoggerFactory.getLogger(Step4IndividualController.class);
 
 	private final static double SMALL_CREDIT_PERCENT = 0.20;
 	private final static double CAR_CREDIT_PERCENT = 0.10;
 	private final static double HOME_CREDIT_PERCENT = 0.20;
-	
+
 	private Step4IndividualWindow window;
 
 	public Step4IndividualController(IndividualModel model) {
@@ -32,7 +32,7 @@ public class Step4IndividualController extends AbstractStepController {
 	@Override
 	public void init() {
 		logger.trace("Calling init()");
-		
+
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -46,53 +46,59 @@ public class Step4IndividualController extends AbstractStepController {
 					@Override
 					public void actionPerformed(ActionEvent arg0) {
 						setToModel(window);
-						if(checkValidness(model)) {
-							window.setVisible(false);
-							Step5IndividualController step4 = new Step5IndividualController(model);
-							step4.init();
-							window.dispose();
+						try {
+							if (checkValidness(model)) {
+								window.setVisible(false);
+								Step5IndividualController step5 = new Step5IndividualController(model);
+								step5.init();
+								window.dispose();
+							}
+						} catch (IllegalArgumentException e) {
+							logger.warn(e.getMessage());
+							ErrorWindow error = new ErrorWindow(e.getMessage());
+							error.setVisible(true);
 						}
 					}
 				});
 			}
 		});
-		
+
 		logger.trace("Returning from init()");
 	}
 
 	public void setToModel(Step4IndividualWindow window) {
 		logger.trace("Calling setToModel({})", window);
-		
+
 		if (window.cbCreditType.getSelectedItem() == CreditType.SHORT_TERM) {
-			
+
 			logger.trace("CreditType == SHORT_TERM");
-			
+
 			this.model.getLastCredit().setProvision(Double.parseDouble(window.tfProvision.getText()));
 			this.model.getLastCredit().setCreditRate(SMALL_CREDIT_PERCENT);
-		} else {			
+		} else {
 			this.model.getLastCredit().setDownPayment(Double.parseDouble(window.tfDownPayment.getText()));
 			if (this.model.getLastCredit().getCreditType() == CreditType.LONG_TERM_CAR) {
-				
+
 				logger.trace("CreditType == LONG_TERM_CAR");
-				
+
 				this.model.getLastCredit().setCreditRate(CAR_CREDIT_PERCENT);
 			} else {
-				
+
 				logger.trace("CreditType == LONG_TERM_HOME");
-				
+
 				this.model.getLastCredit().setCreditRate(HOME_CREDIT_PERCENT);
 			}
 		}
 		this.model.getLastCredit().setCreditType((CreditType) window.cbCreditType.getSelectedItem());
 		this.model.getLastCredit().setCreditSize(Double.parseDouble(window.tfCreditSize.getText()));
 		this.model.getLastCredit().setCreditLength(Double.parseDouble(window.tfCreditLength.getText()));
-		
+
 		logger.trace("Returning from setToModel({})", window);
 	}
-	
-	public boolean checkValidness(IndividualModel model) {
+
+	public static boolean checkValidness(IndividualModel model) throws IllegalArgumentException {
 		logger.trace("Calling checkValidness({})", model);
-		
+
 		double creditSize = model.getLastCredit().getCreditSize();
 		double creditRate = model.getLastCredit().getCreditRate();
 		double creditLength = model.getLastCredit().getCreditLength();
@@ -100,54 +106,53 @@ public class Step4IndividualController extends AbstractStepController {
 		double monthlyIncome = model.getMonthlyIncome();
 		CreditHistory creditHistory = model.getCreditHistory();
 		CreditType creditType = model.getLastCredit().getCreditType();
-		
+
 		boolean result = true;
-		
+
 		if (creditType == CreditType.SHORT_TERM) {
 			logger.trace("CreditType == SHORT_TERM");
-			
+
 			double percentCost = creditSize * creditRate * (creditLength + 1.0) / 24.0;
 			logger.debug("percentCost: {}", percentCost);
-			
+
 			double sum = creditSize + percentCost;
 			logger.debug("sum: {}", sum);
-			
+
 			double netIncome = monthlyIncome * 0.4; // чистый доход
 			logger.debug("netIncome: {}", netIncome);
-			
-			double allNetIncome = netIncome * creditLength; // чистый доход за весь период
+
+			double allNetIncome = netIncome * creditLength; // чистый доход за
+															// весь период
 			logger.debug("allNetIncome: {}", allNetIncome);
-			
+
 			if (allNetIncome < sum) {
-				
+
 				logger.debug("allNetIncome({}) < sum({})", allNetIncome, sum);
-				
-				ErrorWindow error = new ErrorWindow("За " + creditLength + " місяців клієнт може выплатити лише " + allNetIncome + " грн. з " + sum + "грн.");
-				error.setVisible(true);
-				result = false;
+
+				throw new IllegalArgumentException("За " + creditLength + " місяців клієнт може выплатити лише "
+						+ allNetIncome + " грн. з " + sum + "грн.");
 			}
 			if (creditHistory == CreditHistory.IS_REPAID_REGULARLY && provision < (allNetIncome * 2.0)) {
-				
+
 				logger.trace("CreditHistory == IS_REPAID_REGULARLY");
 				logger.debug("provision({}) < 2*allNetIncome({})", provision, (allNetIncome * 2.0));
-				
-				ErrorWindow error = new ErrorWindow("Розмір забезпечення (" + provision + "грн.) повинен бути більше подвійного чистого доходу (" + allNetIncome * 2.0 + " грн.)");
-				error.setVisible(true);
-				result = false;
+
+				throw new IllegalArgumentException("Розмір забезпечення (" + provision
+						+ "грн.) повинен бути більше подвійного чистого доходу (" + allNetIncome * 2.0 + " грн.)");
 			}
 		} else {
-			if (creditType == CreditType.LONG_TERM_CAR) { //машина
-				
+			if (creditType == CreditType.LONG_TERM_CAR) { // машина
+
 				logger.trace("CreditType == LONG_TERM_CAR");
-				
-				if(!calc(model.getLastCredit(), CAR_CREDIT_PERCENT)) {
+
+				if (!calc(model.getLastCredit(), CAR_CREDIT_PERCENT)) {
 					result = false;
 				}
 			} else { // квартира
-				
+
 				logger.trace("CreditType == LONG_TERM_HOME");
-				
-				if(!calc(model.getLastCredit(), HOME_CREDIT_PERCENT)) {
+
+				if (!calc(model.getLastCredit(), HOME_CREDIT_PERCENT)) {
 					result = false;
 				}
 			}
@@ -155,36 +160,36 @@ public class Step4IndividualController extends AbstractStepController {
 		logger.trace("Returning from checkValidness({})", model);
 		return result;
 	}
-	
-	private static boolean calc(CreditModel credit, double downPaymentMinPercent) {
+
+	private static boolean calc(CreditModel credit, double downPaymentMinPercent) throws IllegalArgumentException {
 		logger.trace("Calling from calc({}, {})", credit, downPaymentMinPercent);
-		
+
 		boolean result = true;
-		if(credit.getDownPayment() < credit.getCreditSize() * downPaymentMinPercent) {
-			
-			logger.debug("downPayment({}) < creditSize * downPaymentMinPercent({})", credit.getDownPayment(), (credit.getCreditSize() * downPaymentMinPercent));
-			
-			result = false;
-			ErrorWindow error = new ErrorWindow("Розмір першого взносу (" + credit.getDownPayment() + " грн.) повинен складати 10% від розміру кредиту (" + credit.getCreditSize() + " грн.)");
-			error.setVisible(true);
+		if (credit.getDownPayment() < credit.getCreditSize() * downPaymentMinPercent) {
+
+			logger.debug("downPayment({}) < creditSize * downPaymentMinPercent({})", credit.getDownPayment(),
+					(credit.getCreditSize() * downPaymentMinPercent));
+
+			throw new IllegalArgumentException("Розмір першого взносу (" + credit.getDownPayment()
+					+ " грн.) повинен складати 10% від розміру кредиту (" + credit.getCreditSize() + " грн.)");
 		}
-		double percentCost = (credit.getCreditSize() - credit.getDownPayment()) * credit.getCreditRate() * (credit.getCreditLength() + 1.0) / 24.0;
+		double percentCost = (credit.getCreditSize() - credit.getDownPayment()) * credit.getCreditRate()
+				* (credit.getCreditLength() + 1.0) / 24.0;
 		logger.debug("percentCost: {}", percentCost);
-		
+
 		double sum = credit.getCreditSize() + percentCost;
 		logger.debug("sum: {}", sum);
-		
+
 		if (sum < credit.getNetIncome()) {
-			
+
 			logger.debug("sum < netIncome");
-			
-			result = false;
-			ErrorWindow error = new ErrorWindow("Розмір чистого доходу на місяць (" + credit.getDownPayment() + " грн.) повинен дорівнювати або бути більше суми повернення (" + sum + " грн.)");
-			error.setVisible(true);
+
+			throw new IllegalArgumentException("Розмір чистого доходу на місяць (" + credit.getDownPayment()
+					+ " грн.) повинен дорівнювати або бути більше суми повернення (" + sum + " грн.)");
 		}
 		logger.debug("result: {}", result);
-		
-		logger.trace("Returning from calc({}, {})", credit, downPaymentMinPercent);		
+
+		logger.trace("Returning from calc({}, {})", credit, downPaymentMinPercent);
 		return result;
 	}
 }
